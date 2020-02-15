ac28d56ab9fd9fbd8066264307b9e72bdc5a751f
Jhonny Freitas Dutra <jnydutra@gmail.com>
2020-02-15 20:39:02 -0300

    Dotenv files

--- core/settings.py
+++ core/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '409-@-i1r0h6fw_s$kw8k6e65k#+@7kd)soy!9wh^5t!5-a#nn'
+SECRET_KEY = os.getenv('APP_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("APP_DEBUG")
 
 ALLOWED_HOSTS = []
 
